5b8ebb02bb1d2ce03d60b8830cd56ad48addb208
Kenneth J. McClelland Jr <kxm203@yahoo.com>
2024-03-14 13:05:34 -0400

    I think I got to 100%, the test stops at 69%.

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import desc
 
 from models import db, Bakery, BakedGood
 
@@ -20,19 +21,44 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+
+    response = make_response(
+        bakeries,
+        200,
+        {"Content-Type": "application/json"}
+    )
+    return response
 
 @app.route('/bakeries/<int:id>')
-def bakery_by_id(id):
-    return ''
+def bakery_id(id):
+    bakery_id = Bakery.query.filter(Bakery.id == id).first()
+    bakery_id_dict = bakery_id.to_dict()
+    response = make_response(
+        bakery_id_dict,
+        200,
+        {"Content-Type": "application/json"}
+    )
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods_by_price = db.session.query(BakedGood).join(Bakery).order_by(desc(BakedGood.price)).all()
+    baked_goods_by_price_dict = [baked_good_by_price.to_dict() for baked_good_by_price in baked_goods_by_price]
+    response = jsonify(baked_goods_by_price_dict)
+    response.status_code = 200
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive_baked_good = BakedGood.query.order_by(desc(BakedGood.price)).first()
+    most_expensive_baked_good_dict = most_expensive_baked_good.to_dict()
+    response = make_response(
+        most_expensive_baked_good_dict,
+        200,
+        {"Content-Type": "application/json"}
+    )
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
